c1ebacf271ab62c5f057599b1845bb7cbf59a4ac
Okan Kayas <okan-kayas@web.de>
2018-11-27 16:10:22 +0100

    InfoCell has x and y information now. Cleaned up a bit

--- src/solver.js
+++ src/solver.js
@@ -1,13 +1,15 @@
 import _ from 'lodash'
 import solutions from './solutions'
 
-let finished;
+let finished = false;
 
-function InfoCell(hSum, hCount, vSum, vCount) {
+function InfoCell(x , y, hSum, hCount, vSum, vCount) {
   this.hSum = hSum
   this.hCount = hCount
   this.vSum = vSum
   this.vCount = vCount
+  this.x = x
+  this.y = y
 }
 
 function WhiteCell() {
@@ -17,56 +19,132 @@ function WhiteCell() {
 }
 
 var field = [
-  [null, null, new InfoCell(0, 0, 6, 3), new InfoCell(0, 0, 4, 2)],
-  [null, new InfoCell(3, 2, 3, 2), new WhiteCell(), new WhiteCell()],
-  [new InfoCell(6, 3, 0, 0), new WhiteCell(), new WhiteCell(), new WhiteCell()],
-  [new InfoCell(4, 2, 0, 0), new WhiteCell(), new WhiteCell(), null]
+  [null, null, new InfoCell(0, 2, 0, 0, 6, 3), new InfoCell(0, 3, 0, 0, 4, 2)],
+  [null, new InfoCell(1, 1, 3, 2, 3, 2), new WhiteCell(), new WhiteCell()],
+  [new InfoCell(2, 0, 6, 3, 0, 0), new WhiteCell(), new WhiteCell(), new WhiteCell()],
+  [new InfoCell(3, 0, 4, 2, 0, 0), new WhiteCell(), new WhiteCell(), null]
 ]
 
-function setEntriesForWhiteCells(infoCell, x, y) {
-  for (let i = 1; i <= infoCell.hCount; i++) {
-    field[x][y+i].horizontalEntries =
-      _.intersection(
-        field[x][y+i].horizontalEntries,
-        _.flatten(solutions[infoCell.hSum][infoCell.hCount])
-      )
-  }
-  for (let j = 1; j <= infoCell.vCount; j++) {
-    field[x+j][y].verticalEntries =
-      _.intersection(
-        field[x+j][y].verticalEntries,
-        _.flatten(solutions[infoCell.vSum][infoCell.vCount])
-      )
-  }
+function printCellInfo(cell, infoCell) {
+  console.log("infoCell: ", infoCell)
+  console.log("hEnt: ", cell.horizontalEntries)
+  console.log("vEnt: ", cell.verticalEntries)
+  console.log("value: ", cell.value)
+  console.log("---------------")
 }
 
-function calcPossibilitiesSetValue(cell) {
-  cell.horizontalEntries = cell.verticalEntries =
-    _.intersection(cell.horizontalEntries, cell.verticalEntries)
-
-  cell.value =
-    cell.horizontalEntries.length === 1
-      ? cell.horizontalEntries[0]
-      : 0
-}
-
-function solve() {
-  finished = false
+function initEntries() {
+  console.log('INIT')
 
   for (let x = 0; x < field.length; x++) {
     for (let y = 0; y < field[0].length; y++) {
       const cell = field[x][y]
 
       if (cell instanceof InfoCell) {
-        setEntriesForWhiteCells(cell, x, y)
-      } else if (cell instanceof WhiteCell) {
-        calcPossibilitiesSetValue(cell)
+        for (let i = 1; i <= cell.hCount; i++) {
+          field[x][y+i].horizontalEntries =
+            _.intersection(
+              field[x][y+i].horizontalEntries,
+              _.flatten(solutions[cell.hSum][cell.hCount])
+            )
+          printCellInfo(field[x][y+i], cell)
+        }
+        for (let j = 1; j <= cell.vCount; j++) {
+          field[x+j][y].verticalEntries =
+            _.intersection(
+              field[x+j][y].verticalEntries,
+              _.flatten(solutions[cell.vSum][cell.vCount])
+            )
+            printCellInfo(field[x+j][y], cell)
+        }
       }
+    }
+  }  
+
+  console.log('//////////////////')
+}
+
+function setEntriesForWhiteCells(infoCell) {
+  for (let i = 1; i <= infoCell.hCount; i++) {
+    const whiteCell = field[infoCell.x][infoCell.y+i]
+    if (!whiteCell.value)
+      calcValue(whiteCell, infoCell, 'h')
+  }
+  for (let j = 1; j <= infoCell.vCount; j++) {
+    const whiteCell = field[infoCell.x+j][infoCell.y]
+    if (!whiteCell.value)
+      calcValue(whiteCell, infoCell, 'v')
+  }
+}
+
+function calcValue(cell, infoCell, direction) {
+  if (direction === 'h') {
+    for (let i = 1; i <= infoCell.hCount; i++) {
+      cell.horizontalEntries =
+        _.pull(
+          cell.horizontalEntries,
+          field[infoCell.x][infoCell.y+i].value
+        )
+    }
+  } else {
+    for (let j = 1; j <= infoCell.vCount; j++) { 
+      cell.verticalEntries =
+        _.pull(
+          cell.verticalEntries,
+          field[infoCell.x+j][infoCell.y].value
+        )
+    }
+  }
+  
+  cell.horizontalEntries = cell.verticalEntries =
+    _.intersection(cell.horizontalEntries, cell.verticalEntries)
+
+
+  if (cell.horizontalEntries.length === 1) {
+    cell.value = cell.horizontalEntries[0]
+  } else {
+    finished = false
+  }
+
+  printCellInfo(cell, infoCell)
+}
+
+function solve() {
+  let i = 1
+
+  while (!finished) {
+    finished = true
+
+    for (let x = 0; x < field.length; x++) {
+      for (let y = 0; y < field[0].length; y++) {
+        const cell = field[x][y]
+  
+        if (cell instanceof InfoCell) {
+          setEntriesForWhiteCells(cell)
+        }
+      }
+    }
+
+    // For testing purpose
+    if (i === 10)
+      finished = true
+    ++i
+    console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+  }
+  console.log('Loops: ', i)
+}
+
+function logField() {
+  for (let x = 0; x < field.length; x++) {
+    for (let y = 0; y <field[0].length; y++) {
+      console.log(field[x][y])
     }
   }
 }
 
 export default () => {
+  initEntries()
   solve()
-  console.log(field)
+
+  logField()
 }
