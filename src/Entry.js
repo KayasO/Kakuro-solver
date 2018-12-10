16b72a3d77a21f4eab03755d3ee20c922710f677
Okan Kayas <okan-kayas@web.de>
2018-12-10 19:17:17 +0100

    Moved predefined board setups to boardSetup.js. Also moved Entry, WhiteCell classes to their own files.

---
+++ src/Entry.js
@@ -0,0 +1,10 @@
+export class Entry {
+  constructor(sumH, sumV) {
+    this.sumH = sumH;
+    this.sumV = sumV;
+    this.cellsH = [];
+    this.cellsV = [];
+    this.solutionSetsH = [];
+    this.solutionSetsV = [];
+  }
+}
