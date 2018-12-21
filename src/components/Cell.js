5d8a291ab498c523ccf39eedec085277da84626f
Okan Kayas <okan-kayas@web.de>
2018-12-21 21:02:49 +0100

    Added some styling to EntryCell.js, Cell.js.

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -6,7 +6,12 @@ import styled from 'styled-components'
 const WhiteCell = styled(TableCell)`
   background-color: ${props => (props.children ? lightGreen['A400'] : 'white')};
   color: ${indigo[500]} !important;
+  text-align: center !important;
   font-size: 1.5em !important;
+
+  padding: 0 !important;
+  min-width: 60px !important;
+  height: 60px !important;
 `
 
 export default ({ classes, value }) => {
--- src/components/EntryCell.js
+++ src/components/EntryCell.js
@@ -1,11 +1,42 @@
 import React from 'react'
 import { TableCell } from '@material-ui/core'
+import { grey } from '@material-ui/core/colors'
+import styled from 'styled-components'
 
-// TODO: Split Cell in two halves
-export default ({ classes, below, right }) => {
+import img from '../resources/line_45_007_black.png'
+
+const EntryCell = styled(TableCell)`
+  background-color: ${grey[400]};
+  background-image: url(${img});
+  background-size: contain;
+  background-repeat: no-repeat;
+
+  text-align: center !important;
+  font-size: 1em !important;
+  position: relative;
+
+  padding: 0 !important;
+  min-width: 60px !important;
+  height: 60px !important;
+`
+
+const BottomLeft = styled.span`
+  position: absolute;
+  bottom: 5px;
+  left: 5px;
+`
+
+const TopRight = styled.span`
+  position: absolute;
+  top: 5px;
+  right: 5px;
+`
+
+export default ({ classes, vertical, horizontal }) => {
   return (
-    <TableCell className={classes.tableCell}>
-      {below}\{right}
-    </TableCell>
+    <EntryCell className={classes.tableCell}>
+      <BottomLeft>{vertical}</BottomLeft>
+      <TopRight>{horizontal}</TopRight>
+    </EntryCell>
   )
 }
